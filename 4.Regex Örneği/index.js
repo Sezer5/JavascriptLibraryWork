159ac45dd96f4692d13bf9945ac8c05da50a9fdb
Sezer5 <unalmissezer@gmail.com>
2025-02-24 17:03:50 +0300

    Until Lesson 45

--- 4.Regex Örneği/index.js
+++ 4.Regex Örneği/index.js
@@ -1,13 +1,14 @@
 let signupForm = document.querySelector('.signupForm');
 let username = document.querySelector('#username');
+let message = document.querySelector('.message');
 const pattern = /^[a-z]{6,10}$/;
 signupForm.addEventListener('submit',e=>{
     e.preventDefault();
     let sonuc = pattern.test(signupForm.username.value);
     if(sonuc){
-        alert('Başarılı');
+        message.textContent='Başarılı';
     }else{
-        alert('Başarısız');
+        message.textContent='Başarısız';
     }
 })
 
